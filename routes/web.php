d499fb688f3ace82e9e305cf97bf42311253eb48
Mohamed Fathi <mohamedfathidev161@gmail.com>
2025-03-04 17:04:49 +0200

    theme and localization

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 Route::get('/', function () {
     return view('dashboard');
 });
 Route::resource('students', StudentController::class);
+
+Route::group(['prefix' => LaravelLocalization::setLocale()], function()
+{
+    /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
+    Route::get('/', function()
+    {
+        return view('/dashboard');
+    });
+
+});
